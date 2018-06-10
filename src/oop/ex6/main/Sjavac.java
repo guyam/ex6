231516d75368d2246e2e1aa3dca21b664a00de1a
guyam <39163977+guyam@users.noreply.github.com>
2018-06-10 20:55:51 +0300

    Update Sjavac.java

--- src/oop/ex6/main/Sjavac.java
+++ src/oop/ex6/main/Sjavac.java
@@ -1,4 +1,55 @@
 package oop.ex6.main;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.*;
+import java.util.regex.*;
+
 public class Sjavac {
+
+    public static void main(String[] args) throws IOException {
+        String filePath = args[0];
+        // .mathces();
+        //
+        //String filePath = "testFile";
+        File file = new File(filePath);
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(filePath);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuffer stringBuffer = new StringBuffer();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                Pattern whiteSpacePattern = Pattern.compile("\\s*");
+                Pattern commentLinePattern = Pattern.compile("\\\\");
+                Matcher lineSpace = whiteSpacePattern.matcher((line));
+                Matcher commentLine = commentLinePattern.matcher((line));
+                if (line.equals("")||commentLine.lookingAt()) {
+                    System.out.println("STRING");
+                }
+                else{ // NO MACH
+                    System.out.println(line);
+                }
+
+
+                // if (line.matches("\\s*")){
+            }
+            //if Pattern.
+            //System.out.println(line);
+        }
+
+
+         catch (IOException e) { //TODO CHANGE
+            e.printStackTrace();
+            return;
+
+
+
+
+        }
+
+
+    }
+
 }
