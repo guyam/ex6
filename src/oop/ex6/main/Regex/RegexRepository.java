1d83bb21326b4fa69f915146da5fe93c03029413
GuyA <guy.amir150@gmial.com>
2018-06-11 22:27:28 +0300

    a

--- src/oop/ex6/main/Regex/RegexRepository.java
+++ src/oop/ex6/main/Regex/RegexRepository.java
@@ -9,16 +9,26 @@ public class RegexRepository {
     String[] classNames = {"String", "double", "int", "boolean", "char"};
     public static final Pattern finalTypeRegex = Pattern.compile("^final\\s.*;$");
     public static final Pattern assignmentTypeRegex = Pattern.compile("");
+    public static final  Pattern generalStructureRegex = Pattern.compile("(int|boolean|double|char|String)" +
+            "((.)+);$");
+    public static Matcher generalStructureMatcher;
+    public static final Pattern nameRegex = Pattern.compile(" *(([A-Za-z]+\\w*)|(_+\\w*)) *");
+    public static final Pattern nameRegexWithValue = Pattern.compile(" *(([A-Za-z]+\\w*)|(_+\\w*)) *(=) *" +
+            "(\\w+) *");
+
+
+
 
     //String static final
 
     RegexRepository(String input) {
         this.inputString = input;
+        this.generalStructureMatcher = this.generalStructureRegex.matcher(inputString);
     }
 
 
     boolean checkGeneralValidity() {
-        if (inputString.matches("(int|boolean|double|char|String) .+;$")) {
+        if (generalStructureMatcher.matches()){
             return true;
         }
         //  TODO -- NOAM - DO YOU THINK TO CHECK for 2 ";" or to int/float/bools...?
@@ -27,8 +37,10 @@ public class RegexRepository {
 
 
     boolean checkSingleBlock() {
+        Matcher nameMatcher = nameRegex.matcher(generalStructureMatcher.group(2));
+        Matcher nameMatcherWithValue = nameRegexWithValue.matcher(generalStructureMatcher.group(2));
         if (!inputString.contains("=")) { // if has no =
-            if (inputString.matches("^(int|boolean|double|char|String) (([A-Za-z]+\\w*)|(_+\\w)) \\w+;")) {
+            if (nameMatcher.matches()){
                 // TODO NOAM - call constructor without =
                 System.out.println("BOOM");
                 return true;
@@ -36,18 +48,19 @@ public class RegexRepository {
             return false;
         }
         // next - if more than one == - than false
-            if(inputString.split("=").length >2){
-                return false;
+        if(inputString.split("=").length >2){
+            return false;
         } //made it here - has one "="
-            if(inputString.matches("^(int|boolean|double|char|String) (([A-Za-z]+\\w*)|(_+\\w)) = .+;")) {
+        if(nameMatcherWithValue.matches()) {
             // TODO NOAM - call constructor with =
+            System.out.println("BOOM-222");
             return true;
         }
         return false;
     }
 
 
-    boolean checkMultiBlocks(){
+    boolean checkMultiBlocks(){ //TODO  - WILL CONTINUE NOAM
         return true;
     }
 
@@ -61,40 +74,27 @@ public class RegexRepository {
         return checkMultiBlocks();
 
     }
-        //return checkMultiBlocks(); // TODO MULTI BLOCKS
+    //return checkMultiBlocks(); // TODO MULTI BLOCKS
 
 //
 //
-//            return true;
-//        } // if made it here it has at least one comma
-//        String[] commaDivider = inputString.split(",");
-//        for (String str: commaDivider){
-//            System.out.println(str);
-//        }
-//
-//
-//        System.out.println("the input is: "); // todo del
-//        System.out.println(inputString);
-//        System.out.println(" ");
-//        System.out.println("after division: "); // todo del
-//        //if (inputString.contains("=")){
-////           String[] eqaulsDivider = inputString.split(",");
-////           for (String str: eqaulsDivider){
-////               System.out.println(str);
-////           }
-//           //System.out.println(eqaulsDivider) ; // todo del
-////        }
-//        return true;
-//    }
+
 
     public static void main(String[] args) {
         //String s1 = "int a1 = 7, a2, a3 = 6;";
-        String s2 = "int a1__ = dfghdfgj;";
-        String s3 = "sdfsd= =sdffd";
-        System.out.println(s3.split("=").length);
+        String s2 = "int                         1_1_=1;";
+        //String s3 = "sdfs= =sdffd";
+        // System.out.println(s3.split("=").length);
         RegexRepository r2 = new RegexRepository(s2);
         //r1.checkValidity();
         //System.out.println(r1.checkGeneralValidity());
+        System.out.println(r2.checkGeneralValidity());
+        int i = 0;
+//        while (r2.generalStructureMatcher.find()) {
+//            System.out.println(r2.generalStructureMatcher.group(i));
+//            i++;
+//        }
+        System.out.println(r2.generalStructureMatcher.group(2));
         System.out.println(r2.checkSingleBlock());
     }
 
