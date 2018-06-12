d5f5f4d45f617cee8946b5241fbcc23c4bc95f79
GuyA <guy.amir150@gmial.com>
2018-06-12 21:43:29 +0300

    11

--- src/oop/ex6/main/Regex/RegexRepository.java
+++ src/oop/ex6/main/Regex/RegexRepository.java
@@ -1,4 +1,6 @@
 package oop.ex6.main.Regex;
+import oop.ex6.main.Types.JavaType;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,9 +14,10 @@ public class RegexRepository {
     public static final  Pattern generalStructureRegex = Pattern.compile(" *(final)? *(int|boolean|double|char|String)" +
             "((.)+);$");
     public static Matcher generalStructureMatcher;
-    public static final Pattern nameRegex = Pattern.compile(" *(([A-Za-z]+\\w*)|(_+\\w*)) *");
-    public static final Pattern nameRegexWithValue = Pattern.compile(" *(([A-Za-z]+\\w*)|(_+\\w*)) *(=) *" +
-            "(\\w+) *");
+    public static final Pattern nameRegex = Pattern.compile(" *(([A-Za-z]+[A-Za-z_0-9]*)|(_[A-Za-z_0-9]+)|" +
+            "(__[A-Za-z_0-9]*)) *");
+    public static final Pattern nameRegexWithValue = Pattern.compile(" *(([A-Za-z]+[A-Za-z_0-9]*)|" +
+            "(_[A-Za-z_0-9]+)|(__[A-Za-z_0-9]*)) *(=) *" +"(\\w+) *");
 
 
 
@@ -41,8 +44,12 @@ public class RegexRepository {
         Matcher nameMatcherWithValue = nameRegexWithValue.matcher(generalStructureMatcher.group(3));
         if (!inputString.contains("=")) { // if has no =
             if (nameMatcher.matches()){
-                // TODO NOAM - call constructor without =
-                System.out.println("BOOM");
+                if (generalStructureMatcher.group(1)!=null){ // if the is 'final' but not =
+                    return false;
+                }
+                JavaType globalLine = new JavaType(generalStructureMatcher.group(2));
+                System.out.println(nameMatcher.group(1)); // todo NOAM  del - name only
+                System.out.println("BOOM");  // todo del
                 return true;
             }
             return false;
@@ -61,8 +68,32 @@ public class RegexRepository {
 
 
     boolean checkMultiBlocks(){ //TODO  - WILL CONTINUE NOAM
+        //Matcher nameMatcher = nameRegex.matcher(generalStructureMatcher.group(3));
+        String lineWithComma = generalStructureMatcher.group(3);
+        String[] dividedStringArray = lineWithComma.split(","); // TODO MAKE SURE ASSUMPTION COMMA IS CORRECT
+        //System.out.println(lineWithComma);
+        for (String subString: dividedStringArray){ //TODO DEL
+            //System.out.println(subString);
+            if (!subStringNameChecker(subString)){
+                return false;
+            } // made it here - all name ok
+        }
+        //System.out.println(dividedStringArray);  //TODO DEL
         return true;
     }
+
+
+    boolean subStringNameChecker(String subString){
+        Matcher subStringnameMatcher = nameRegex.matcher(subString);
+        Matcher subStringnameMatcherWithValue = nameRegexWithValue.matcher(subString);
+        if (!subString.contains("=")){ // with no '='
+            //System.out.println("no ="); // TODO DEL
+            return subStringnameMatcher.matches();
+        }
+        //System.out.println("YESSS ="); // TODO DEL
+        return subStringnameMatcherWithValue.matches();
+    }
+
 
     boolean checkSyntaxValidity () {
         if (!checkGeneralValidity()) {
@@ -82,23 +113,24 @@ public class RegexRepository {
 
     public static void main(String[] args) {
         //String s1 = "int a1 = 7, a2, a3 = 6;";
-        String s2 = "        final       int                         1a1_1_1;";
+        String s2 = "  final    int        a=45     ,    a_, y =8__,      a1a1=_1_1;";
         //String s3 = "sdfs= =sdffd";
         // System.out.println(s3.split("=").length);
         RegexRepository r2 = new RegexRepository(s2);
         //r1.checkValidity();
-        //System.out.println(r1.checkGeneralValidity());
-        System.out.println(r2.checkGeneralValidity());
-        int i = 0;
+        //System.out.println(r2.checkGeneralValidity());
+        //System.out.println(r2.checkSyntaxValidity());
+        //int i = 0;
 //        while (r2.generalStructureMatcher.find()) {
 //            System.out.println(r2.generalStructureMatcher.group(i));
 //            i++;
 //        }
-        System.out.println(r2.checkGeneralValidity());
-        System.out.println(r2.generalStructureMatcher.group(1));
-        System.out.println(r2.generalStructureMatcher.group(2));
-        System.out.println(r2.generalStructureMatcher.group(3));
-        System.out.println(r2.checkSingleBlock());
+        System.out.println(r2.checkSyntaxValidity());
+//        System.out.println(r2.generalStructureMatcher.group(1));
+//        System.out.println(r2.generalStructureMatcher.group(2));
+//        System.out.println(r2.generalStructureMatcher.group(3));
+        //System.out.println(r2.checkGeneralValidity());
+        //System.out.println(r2.checkMultiBlocks());
     }
 
 
