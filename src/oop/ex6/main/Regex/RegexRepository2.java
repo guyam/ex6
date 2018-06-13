629a778e38c7128d2ff2f2426153171bc39eaa38
normanfish <noamf007@gmail.com>
2018-06-13 09:03:50 +0300

    1.0

--- src/oop/ex6/main/Regex/RegexRepository2.java
+++ src/oop/ex6/main/Regex/RegexRepository2.java
@@ -3,4 +3,5 @@ package oop.ex6.main.Regex;
 public class RegexRepository2 {
 
     final String functionTitleRegex = "[ \\t\\r]*void[ \\t\\r]+([a-zA-Z]+\\w?)[ \\t\\r]*\\((.*)\\)[ \\t\\r]*\\{";
+    final String functionCallRegex = "[ \\t\\r]*([a-zA-Z]+\\w?)[ \\t\\r]*\\(.*[ \\t\\r]*;$";
 }
