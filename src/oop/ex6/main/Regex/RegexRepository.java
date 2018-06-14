d716c3dd40aa5bdb7b8221e298e7e8b28a6ccd0b
guyam <39163977+guyam@users.noreply.github.com>
2018-06-14 23:36:31 +0300

    Update RegexRepository.java

--- src/oop/ex6/main/Regex/RegexRepository.java
+++ src/oop/ex6/main/Regex/RegexRepository.java
@@ -6,6 +6,7 @@ import oop.ex6.main.Types.FunctionType;
 import oop.ex6.main.Types.JavaType;
 
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -91,24 +92,37 @@ public class RegexRepository {
         String parameterList = methodGeneralNameMatcher.group(4);
         boolean parameterlessMethod = parameterList.matches("[ \\t\\r]*");
         if (parameterlessMethod){
-            this.methodSet.put(methodName, new FunctionType(new ArrayList<String>(), new TreeMap<String,
-                    JavaType>()));
+            this.methodSet.put(methodName, new FunctionType(new LinkedHashMap<String, JavaType>()));
             return true; // todo change
         } // TODO !!!! - UPDATE STACK OF "{}"
         if (!parameterList.contains(",")){ // no comma
             String parameters = parameterList+";";
             //System.out.println(parameters); // todo del
-            TreeMap<String, JavaType> localVarSet = new TreeMap<>();
+            LinkedHashMap<String, JavaType> localVarSet = new LinkedHashMap<>();
             RegexRepository methodParameterChecker = new RegexRepository(parameters,localVarSet, null,
                     null, 1);
             //System.out.println(methodParameterChecker.checkSyntaxValidity());
-            methodParameterChecker.checkSyntaxValidity();
-            this.methodSet.put(methodName, new FunctionType(new ArrayList<String>(),localVarSet));
-//            System.out.println(methodName);
-//            System.out.println(localVarSet.get("a1"));
+            methodParameterChecker.checkSyntaxValidity(true);
+            this.methodSet.put(methodName, new FunctionType(localVarSet));
             return true; // TODO CHANGE??
         } // made it here- contains commas
-        //String[] paramlist //TODO NOAM - CONTINUE
+        System.out.println("YOLO"); // todo del
+        LinkedHashMap<String, JavaType> localVars = new LinkedHashMap<>();
+        RegexRepository methodParameterChecker = new RegexRepository("",localVars, null,
+                null, 1);
+        String[] comalessParams = parameterList.split(",");
+        for (String parSubString : comalessParams){
+            String param = parSubString + ";";
+            methodParameterChecker.setString(param);
+            methodParameterChecker.checkSyntaxValidity(true);
+            System.out.println(param); //todo del
+        }
+        this.methodSet.put(methodName, new FunctionType(localVars));
+
+//        System.out.println(parameterList);
+//        System.out.println("A");
+//        System.out.println(comalessParams[0]);
+//        System.out.println(comalessParams[1]);
         return true;
     }
 
@@ -116,63 +130,68 @@ public class RegexRepository {
 
 
 
-    boolean checkSingleBlock(String lineName) throws EmptyFinalDeclarationException {//, VariableAlreadyExistsException {
+    boolean checkSingleBlock(String lineName, boolean isMethodParam) throws EmptyFinalDeclarationException {//,
+        // VariableAlreadyExistsException {
         Matcher nameMatcher = nameRegex.matcher(lineName);
         Matcher nameMatcherWithValue = nameRegexWithValue.matcher(lineName);
 
         if (!lineName.contains("=")) { // if has no =
             if (nameMatcher.matches()) {
-////                // TODO DEBUG
-//                System.out.println("WE GOT");
-//                System.out.println(generalStructureMatcher.group(1));
-////                // TODO DEBUG
                 if (generalStructureMatcher.group(1) != null) { // if the is 'final' but not =
-                    //System.out.println("YES1");
-                    System.out.println("EXCEPTION!!");
-                    // TODO EXCEPTION!!!!!
-                    return false;
+                    if (!isMethodParam) {
+                        System.out.println("EXCEPTION!!");
+                        // TODO EXCEPTION!!!!!
+                        return false;
+                    } // if made it here - its a method
+//                    if (!(variableSet.containsKey(lineName.trim())) && !(variableSet.get(lineName.trim())
+//                            .getScope() == scope)) { // TODO - NOAM IS THIS GOOD???????
+                        System.out.println("BUILD CONSTRUCTOR");
+                        //TODO _ NOAM - CONTINUE TO FINAL PARAMS!!!!
+//                        variableSet.put(var, new JavaType(typeName, val, generalStructureMatcher.group(1) != null, scope));
+                        return true;
+//                    }
                 }
-                if ((variableSet.containsKey(lineName.trim())) && (variableSet.get(lineName.trim()).getScope
-                        () == scope)) {
+                    if ((variableSet.containsKey(lineName.trim())) && (variableSet.get(lineName.trim()).getScope
+                            () == scope)) {
+                        System.out.println("EXCEPTION!!!!!");
+                        // TODO - THROW EXCEPTION
+                        return false;
+                    }
+                    variableSet.put(lineName.trim(), new JavaType(generalStructureMatcher.group(2), scope));
+                    System.out.println("BOOM");  // todo del
+                    return true;
+                }
+                System.out.println("EXCEPTION!!");
+                // TODO EXCEPTION!!!!!
+                return false;
+            }
+            // next - if more than one == - than false
+            if (lineName.split("=").length > 2) {
+                System.out.println("EXCEPTION!!");
+                // TODO EXCEPTION!!!!!
+                return false;
+            } //made it here - has one "="
+            String[] varAndVal = lineName.split("=");
+            boolean isFinal = generalStructureMatcher.group(1) != null;
+            String var = varAndVal[0].trim();
+            String val = varAndVal[1].trim();
+            String typeName = generalStructureMatcher.group(2);
+            if (nameMatcherWithValue.matches()) {
+                if ((variableSet.containsKey(var)) && (variableSet.get(var).getScope() == scope)) {
                     //throw new VariableAlreadyExistsException();
-                    //System.out.println("EXCEPTION"); // todo del;
                 }
-                variableSet.put(lineName.trim(), new JavaType(generalStructureMatcher.group(2), scope));
-                System.out.println("BOOM");  // todo del
+                variableSet.put(var, new JavaType(typeName, val, isFinal, scope));
+                System.out.println("BOOM-222");
                 return true;
             }
             System.out.println("EXCEPTION!!");
             // TODO EXCEPTION!!!!!
             return false;
         }
-        // next - if more than one == - than false
-        if (lineName.split("=").length > 2) {
-            System.out.println("EXCEPTION!!");
-            // TODO EXCEPTION!!!!!
-            return false;
-        } //made it here - has one "="
-        //Matcher subStringWithValue =  subnameRegexWithValue.matcher(lineName);
-        //System.out.println(lineName); // todo chenge
-        String[] varAndVal = lineName.split("=");
-        boolean isFinal = generalStructureMatcher.group(1) != null;
-        String var = varAndVal[0].trim();
-        String val = varAndVal[1].trim();
-        String typeName = generalStructureMatcher.group(2);
-        if (nameMatcherWithValue.matches()) {
-            if ((variableSet.containsKey(var)) && (variableSet.get(var).getScope() == scope)) {
-                //throw new VariableAlreadyExistsException();
-            }
-            variableSet.put(var, new JavaType(typeName, val, isFinal, scope));
-            System.out.println("BOOM-222");
-            return true;
-        }
-        System.out.println("EXCEPTION!!");
-        // TODO EXCEPTION!!!!!
-        return false;
-    }
 
 
-    boolean checkMultiBlocks() throws EmptyFinalDeclarationException { //TODO  - WILL CONTINUE NOAM
+
+    boolean checkMultiBlocks() throws EmptyFinalDeclarationException{ //TODO  - WILL CONTINUE NOAM
         //Matcher nameMatcher = nameRegex.matcher(generalStructureMatcher.group(3));
         String lineWithComma = generalStructureMatcher.group(3);
         String[] dividedStringArray = lineWithComma.split(","); // TODO MAKE SURE ASSUMPTION COMMA IS CORRECT
@@ -186,10 +205,8 @@ public class RegexRepository {
             } // made it here - all name ok
         }
         for (String subString : dividedStringArray) { //TODO DEL
-            checkSingleBlock(subString);
-
+            checkSingleBlock(subString, false);
         }
-        //System.out.println(dividedStringArray);  //TODO DEL
         return true;
     }
 
@@ -206,14 +223,14 @@ public class RegexRepository {
     }
 
 
-    public boolean checkSyntaxValidity() throws EmptyFinalDeclarationException {
+    public boolean checkSyntaxValidity(boolean isMethod) throws EmptyFinalDeclarationException{
 //        try{
         if (!checkGeneralValidity()) {
             return false;
         }
         if (!inputString.contains(",")) { // if contains
             String fullComalessLine = generalStructureMatcher.group(3);
-            return checkSingleBlock(fullComalessLine);
+            return checkSingleBlock(fullComalessLine, isMethod);
         } // has mor than one block
         return checkMultiBlocks();
 //    }
@@ -231,7 +248,10 @@ public class RegexRepository {
     //
     public static void main(String[] args) throws EmptyFinalDeclarationException {
         TreeMap<String, FunctionType> methodMap1 = new TreeMap<>();
-        String s1 = "   void getNoam1  (final int a1=7)      {";
+        String s1 = "   void getNoam1  (final int a1, char b, String c)      {";
+        String[] array1 = s1.split(",");
+        System.out.println(array1);
+        //System.out.println(array1[1:array1.length]);
         RegexRepository r2 = new RegexRepository(s1, null, methodMap1, null, 0);
         System.out.println(r2.checkMethodSyntax());
 
