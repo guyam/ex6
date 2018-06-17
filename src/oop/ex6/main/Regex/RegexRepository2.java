8a2fcef5c2d1bb083f006c9f77d747103f91b63b
guyam <39163977+guyam@users.noreply.github.com>
2018-06-17 21:05:45 +0300

    Update RegexRepository2.java

--- src/oop/ex6/main/Regex/RegexRepository2.java
+++ src/oop/ex6/main/Regex/RegexRepository2.java
@@ -1,50 +1,132 @@
 package oop.ex6.main.Regex;
 
 import oop.ex6.main.Exceptions.EmptyFinalDeclarationException;
-import oop.ex6.main.Exceptions.FinalAssignmentException;
 import oop.ex6.main.Exceptions.VariableAlreadyExistsException;
-import oop.ex6.main.Exceptions.VariableNotDeclaredException;
-import oop.ex6.main.Tuple;
+//import oop.ex6.main.Tuple;
 import oop.ex6.main.Types.*;
 
+
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Stack;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegexRepository2 {
 
     private String inputString;
-    public Stack<Integer> parathensisCounter;
-    public ArrayList<LinkedHashMap<String, JavaType>> variableSet;
+    public TreeMap<String, JavaType> variableSet;
     public TreeMap<String, FunctionType> functionSet;
     public int scope;
+    public static Matcher generalConditionMatcher;
 
 
-    final String functionTitleRegex = "[ \\t\\r]*void[ \\t\\r]+([a-zA-Z]+\\w?)[ \\t\\r]*\\((.*)\\)[ \\t\\r]*\\{";
-    final String functionCallRegex = "[ \\t\\r]*([a-zA-Z]+\\w?)[ \\t\\r]*\\(.*[ \\t\\r]*;$";
 
-    public RegexRepository2(String input, ArrayList<LinkedHashMap<String,JavaType>> varSetInput,
-                            TreeMap<String, FunctionType> funcSetInput, int scopeInput, Stack<Integer> pCounter) {
+    public static final Pattern generalCondition = Pattern.compile("[ \\t\\r]*(if|while)[ \\t\\r]*" +
+            "(\\()[ \\t\\r]*((.)*)[ \\t\\r]*(\\))[ \\t\\r]*(\\{)[ \\t\\r]*");
+    public static final Pattern booleanReservedWord = Pattern.compile("[ \\t\\r]*(true|false)[ \\t\\r]*");
+    public static final Pattern booleanVar = Pattern.compile("[ \\t\\r]*((.)*)[ \\t\\r]*[ " +
+            "\\t\\r]*");
+
+    public RegexRepository2(String input, TreeMap<String, JavaType> varSetInput, TreeMap<String,FunctionType> funcSetInput, int scopeInput) {
         this.inputString = input;
         //TODO - guy, the correct regex matcher
         this.variableSet = varSetInput;
         this.functionSet = funcSetInput;
         this.scope = scopeInput;
-        this.parathensisCounter = pCounter;
+        this.generalConditionMatcher = this.generalCondition.matcher(inputString);
     }
 
     public void update(String input, int scopeInput) {
         scope = scopeInput;
         inputString = input;
+        this.generalConditionMatcher = this.generalCondition.matcher(inputString);
     }
+
+
+    public boolean singleBoolBlockChecker(String singleBlock){
+        Matcher singleReservedWordMatcher = booleanReservedWord.matcher(singleBlock);
+        Matcher singleBoolValMatcher = booleanVar.matcher(singleBlock);
+        if (singleReservedWordMatcher.matches()){
+            System.out.println("TRUE FALSE WORD"); // TODO DEL
+            return true;
+        }
+        if (!singleBoolValMatcher.matches()){
+            return false;
+        }
+        String boolVar = singleBlock.trim();
+        if (variableSet.containsKey(boolVar)){ // it is a variable
+            String boolVarType = variableSet.get(boolVar).getType();
+            if (JavaType.contains(JavaType.compatibleTypes.get("boolean"), boolVarType)){
+                System.out.println("BOOLEAN GLOBAL VARIABLE!"); // TODO DEL
+                return true;
+            }
+            System.out.println("EXCEPTION NOT BOOLEAN VARIABLE"); // TODO ADD EXCEPTION
+            return false;
+        }
+        System.out.println(boolVar);
+        System.out.println("NOT IN DICT");
+        if (JavaType.isDouble(boolVar)||JavaType.isInt(boolVar)){
+            System.out.println("THIS IS AN INT OR A DOUBLE");
+            return true;
+        }
+        System.out.println("EXCEPTION! REACH END! NOT VALID CONDITION"); // TODO ADD EXCEPTION
+        return false;
+    }
+
+
+    public boolean checkBooleanSyntax(){
+        if (!generalValidityChecker()){ // check general syntax
+            System.out.println("BAD BOOLEAN EXCEPTION"); // TODO EXCEPTION
+            return false;
+        }
+        String boolCondInput = this.generalConditionMatcher.group(3);
+        //System.out.println(boolCondInput);
+        if (!boolCondInput.contains("||")&&!boolCondInput.contains("&&")) {
+            System.out.println("ONLY SINGLE BLOCK TO CHECK"); // TODO DEL
+            return singleBoolBlockChecker(boolCondInput);
+        }
+        System.out.println("MULTI BLOCKS");// TODO DEL
+        ArrayList<String> condBlockArray = combinedConditionBlocks(boolCondInput);
+        for (String conditionBlock : condBlockArray){
+            if (singleBoolBlockChecker(conditionBlock)){
+                continue;
+            }
+            System.out.println("EXCEPTION! PROBLEM WITH AT LEAST ONE BLOCK");
+            return false; //TODO ADD EXCEPTION
+        }
+
+        return true; // TODO CHANGE!!!!!
+    }
+
+
+    private ArrayList<String> combinedConditionBlocks(String fullString){
+        String[] orOpStrings = fullString.split("\\|\\|");
+        ArrayList<String> allConditions = new ArrayList<>();
+        for (String str: orOpStrings){
+            if (!str.contains("&&")){
+                allConditions.add(str);
+                continue;
+            }
+            String[] andConds = str.split("(&&)");
+            for (String localString: andConds){
+                allConditions.add(localString);
+            }
+        }
+//        System.out.println("THE SIZE IS");// todo DEL
+//        System.out.println(allConditions.size()); // todo DEL
+//        for (String l: allConditions){ // todo DEL
+//            System.out.println(l); // todo DEL
+//        } // todo DEL
+        return  allConditions;
+    }
+
 
     /**
      * basicly a switchboard, checks to see what sort of line we got with the regex filter, and then handles each
      * case by directing to a different function.
      */
-    public void lineHandler() {
-        ///todo - regex
+    public boolean generalValidityChecker() {
+        return generalConditionMatcher.matches();
     }
 
     /**
@@ -52,10 +134,10 @@ public class RegexRepository2 {
      */
     private void basicDecLine(String name, String type) throws VariableAlreadyExistsException {
         //check if there is already a variable with the same name in the code
-        if (variableSet.get(variableSet.size()).containsKey(name))
+        if (variableSet.containsKey(name))
             throw new VariableAlreadyExistsException();
         // let's add it to the variable set.
-        variableSet.get(variableSet.size()).put(name, new JavaType(type, scope));
+        variableSet.put(name,new JavaType(type, scope));
     }
 
     /**
@@ -63,43 +145,29 @@ public class RegexRepository2 {
      */
     private void basicDecAndInitLine(String name, String type, String data, boolean isFinal) throws VariableAlreadyExistsException, EmptyFinalDeclarationException {
         //check if there is already a variable with the same name in the code
-        if (variableSet.get(variableSet.size()).containsKey(name))
+        if (variableSet.containsKey(name))
             throw new VariableAlreadyExistsException();
-        variableSet.get(variableSet.size()).put(name, new JavaType(type, data, isFinal, scope));
+        variableSet.put(name, new JavaType(type, data, isFinal, scope));
     }
 
-    /**
-     * this function handles an assignment to a variable
-     */
-    private void basicassignmentLine(String varName, String newValue) throws FinalAssignmentException, VariableNotDeclaredException {
-        //first, we'll check all of the scopes, from the most recent to the first, and check if the variable already exists
-        for (int i = variableSet.size() - 1; i >= 0; i--) {
-            //now that we are in the scope's variable dictionary, we'll check if a variable with the same name exists in this scope
-            if (variableSet.get(i).containsKey(varName)) {
-                // try to assign the value. will not assign if it's final and throws an exception
-                variableSet.get(i).get(varName).update(newValue);
-                //if we reached here, the assignment was succesful. we can exit the function
-                return;
-            }
 
-        }
-        //there is no variable with this name, we will therfor throw an exception
-        throw new VariableNotDeclaredException();
+    private void basicassignmentLine() {
+
+
     }
 
-    public int getScope() {
-        return scope;
-    }
 
-    private void functionDecType(String functionName){
-        //increase the stack that counts the parenthasis and the scope we are going into
-        parathensisCounter.push(0);
-        scope++;
-        //add the function parameters to the Set of javaTypes, we just opened up a new scope, so we don't need to worry
-        // about clashes
-        variableSet.add(new LinkedHashMap<>());
-        variableSet.get(variableSet.size()-1).putAll(functionSet.get(functionName).getParameters());
-    }
+    public static void main(String args[]){
+        JavaType var1 = new JavaType("boolean", 0);
+        String s1 = "   while (      a12||  a12  )  {  " ;
+        TreeMap<String, JavaType> variableDictTest = new TreeMap<>();
+        variableDictTest.put("a12", var1);
+        RegexRepository2 r2 = new RegexRepository2(s1, variableDictTest, null, 0);
+        System.out.println(r2.checkBooleanSyntax());
+        //System.out.println(r2.generalConditionMatcher.group(1));
+        //System.out.println(r2.generalConditionMatcher.group(2));
+       // System.out.println(r2.generalConditionMatcher.group(3));
 
+    }
 
 }
